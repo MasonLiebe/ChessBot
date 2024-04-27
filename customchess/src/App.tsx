0389e9d0f2ff653b1e8e5e6a784f6a2e0449d2d2
MasonLiebe <149519733+MasonLiebe@users.noreply.github.com>
2024-04-27 13:12:46 +0900

    navbar

--- customchess/src/App.tsx
+++ customchess/src/App.tsx
@@ -1,14 +1,39 @@
 import React, { useState } from 'react';
 import './App.css';
-import PieceCustomizer from './components/PieceCustomizer/PieceCustomizer';
-import PiecePanel from './components/PiecePanel/PiecePanel';
-import CustomPieceSet from './components/CustomPIeceSet/CustomPieceSet';
-import { GameWorkshop } from './components/GameWorkshop/GameWorkshop';
 import { PieceWorkshop } from './components/PieceWorkshop/PieceWorkshop';
+import { GameWorkshop } from './components/GameWorkshop/GameWorkshop';
 
 function App() {
-  return PieceWorkshop();
+  const [activeWorkshop, setActiveWorkshop] = useState('piece');
+
+  const handleWorkshopChange = (workshop: React.SetStateAction<string>) => {
+    setActiveWorkshop(workshop);
+  };
+
+  return (
+    <div className="app">
+      <nav className="navbar">
+        <div className="navbar-brand">Custom Chess</div>
+        <ul className="navbar-nav">
+          <li
+            className={`nav-item ${activeWorkshop === 'piece' ? 'active' : ''}`}
+            onClick={() => handleWorkshopChange('piece')}
+          >
+            Piece Workshop
+          </li>
+          <li
+            className={`nav-item ${activeWorkshop === 'game' ? 'active' : ''}`}
+            onClick={() => handleWorkshopChange('game')}
+          >
+            Game Workshop
+          </li>
+        </ul>
+      </nav>
+      <div className="workshop-container">
+        {activeWorkshop === 'piece' ? <PieceWorkshop /> : <GameWorkshop />}
+      </div>
+    </div>
+  );
 }
 
 export default App;
-
